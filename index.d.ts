fecbbf2813582eed13fc00ac90790e84f013fca3
dzface <dzface031@gmail.com>
2025-01-06 00:20:11 +0900

    export default 로 설정하여 {} 안생기게 호출

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 //index.d.ts
 
 declare module 'react-native-keyboard-responsive-value' {
-export function flexibleValue(defaultValue: string | number, onKeyboardValue : string | number): string | number;
+export default function flexibleValue(defaultValue: string | number, onKeyboardValue : string | number): string | number;
 }
 
